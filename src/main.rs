0529ec529642f38001c22c994f9047caa6cb30d3
Arash-11 <arashnawy@gmail.com>
2023-03-11 13:59:04 -0500

    Built dungeon building blocks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+
 use tcod::system;
 use tcod::colors::*;
 use tcod::console::*;
@@ -54,21 +56,6 @@ impl Object {
     }
 }
 
-fn handle_keys(object: &mut Object, game: &Game, key_code: KeyCode) -> bool {
-    match key_code {
-        KeyCode::Up => object.move_by(0, -1, game),
-        KeyCode::Down => object.move_by(0, 1, game),
-        KeyCode::Left => object.move_by(-1, 0, game),
-        KeyCode::Right => object.move_by(1, 0, game),
-
-        KeyCode::Escape => return true,
-
-        _ => ()
-    };
-
-    false
-}
-
 /// A tile of the map and its properties
 #[derive(Clone)]
 struct Tile {
@@ -76,30 +63,88 @@ struct Tile {
     block_sight: bool,
 }
 
+/// A rectangle on the map. Used to characterize a room.
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x + w,
+            y2: y + h,
+        }
+    }
+}
+
 type Map = Vec<Vec<Tile>>;
 struct Game {
     map: Map,
 }
 
-/// Fill map with "unblocked" tiles
 fn make_map() -> Map {
-    vec![vec![Tile{ blocked: false, block_sight: false }; MAP_HEIGHT as usize]; MAP_WIDTH as usize]
+    // Fill map with "blocked" tiles
+    let mut map = vec![
+        vec![Tile{ blocked: true, block_sight: true }; MAP_HEIGHT as usize];
+        MAP_WIDTH as usize
+    ];
+
+    let room1 = Rect::new(20, 15, 10, 15);
+    let room2 = Rect::new(50, 15, 10, 15);
+
+    create_room(&mut map, room1);
+    create_room(&mut map, room2);
+
+    create_h_tunnel(&mut map, 25, 55, 23);
+
+    map
+}
+
+/// Go through the tiles in the rectangle and make them passable
+fn create_room(map: &mut Map, room: Rect) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize].blocked = false;
+            map[x as usize][y as usize].block_sight = false;
+        }
+    }
+}
+
+/// Create horizontal tunnel
+fn create_h_tunnel(map: &mut Map, x1: i32, x2: i32, y: i32) {
+    for x in cmp::min(x1, x2)..cmp::max(x1, x2) {
+        map[x as usize][y as usize].blocked = false;
+        map[x as usize][y as usize].block_sight = false;
+    }
+}
+
+/// Create vertical tunnel
+fn create_v_tunnel(map: &mut Map, y1: i32, y2: i32, x: i32) {
+    for y in cmp::min(y1, y2)..cmp::max(y1, y2) {
+        map[x as usize][y as usize].blocked = false;
+        map[x as usize][y as usize].block_sight = false;
+    }
 }
 
 /// Draw all objects in the list
-fn render_all(tcod: &mut Tcod, game: &mut Game, objects: &[Object; 2]) {
+fn render_all(tcod: &mut Tcod, map: &mut Map, objects: &[Object; 2]) {
     for object in objects {
         object.draw(&mut tcod.con);
     }
 
-    game.map[30][22].blocked = true;
-    game.map[30][22].block_sight = true;
-    game.map[50][22].blocked = true;
-    game.map[50][22].block_sight = true;
+    map[30][22].blocked = true;
+    map[30][22].block_sight = true;
+    map[50][22].blocked = true;
+    map[50][22].block_sight = true;
 
     for y in 0..MAP_HEIGHT {
         for x in 0..MAP_WIDTH {
-            let wall = game.map[x as usize][y as usize].block_sight;
+            let wall = map[x as usize][y as usize].block_sight;
 
             match wall {
                 true => tcod.con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set),
@@ -117,6 +162,22 @@ fn render_all(tcod: &mut Tcod, game: &mut Game, objects: &[Object; 2]) {
         1.0,
         1.0
     );
+}
+
+/// Handle key events from user
+fn handle_keys(object: &mut Object, game: &Game, key_code: KeyCode) -> bool {
+    match key_code {
+        KeyCode::Up => object.move_by(0, -1, game),
+        KeyCode::Down => object.move_by(0, 1, game),
+        KeyCode::Left => object.move_by(-1, 0, game),
+        KeyCode::Right => object.move_by(1, 0, game),
+
+        KeyCode::Escape => return true,
+
+        _ => ()
+    };
+
+    false
 }
 
 fn main() {
@@ -142,10 +203,14 @@ fn main() {
         map: make_map()
     };
 
+    let player = &mut objects[0];
+    player.x = 25;
+    player.y = 23;
+
     while !tcod.root.window_closed() {
         tcod.con.clear();
 
-        render_all(&mut tcod, &mut game, &objects);
+        render_all(&mut tcod, &mut game.map, &objects);
 
         tcod.root.flush();
 
